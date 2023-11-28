a057ab0caa7b52a1cb82eb1ac51042eeee840ee7
nehalhassan <nh.elsaify@gmail.com>
2023-11-28 22:14:30 +0200

    add body parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const api = require("./api");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use("/api", api);
 
