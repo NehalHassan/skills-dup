0960dc2f14d6a6886f67ad72ae79294d601643e5
nehalhassan <nh.elsaify@gmail.com>
2023-11-28 22:35:45 +0200

    try to fix cors error with post

--- src/app.js
+++ src/app.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api", api);
 
