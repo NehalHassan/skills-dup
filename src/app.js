94869cf33316a290567c814f9f25a1c19059148f
nehalhassan <nh.elsaify@gmail.com>
2023-11-29 13:32:01 +0200

    allow cors

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const api = require("./api");
-
+const allowCors = require("./allow-cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+app.use(allowCors());
 app.use("/api", api);
 
 module.exports = app;
